3bb5b965c4acb5284e52a12c10eeb5bbd18bc47a
ramoniswack <ramontiwari086@gmail.com>
2026-07-07 13:47:45 +0000

    fix

--- app/modules/favorites/api.py
+++ app/modules/favorites/api.py
@@ -150,7 +150,7 @@ async def favorite_menu_item(
             ),
         )
         return ApiResponse(message="Menu item saved to favorites.", data=data)
-    except Exception as e:
+    except Exception:
         import traceback
         error_detail = traceback.format_exc()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error_detail)
