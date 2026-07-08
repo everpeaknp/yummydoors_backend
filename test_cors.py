b02a040fdab574acac5239b080806b04fe66140f
ramoniswack <ramontiwari086@gmail.com>
2026-07-08 11:21:13 +0000

    Remove accidental scratch scripts from backend repo

--- test_cors.py
+++
@@ -1,5 +0,0 @@
-from starlette.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000", "*"], allow_credentials=True)
-print("Middleware added successfully!")
--- test_req.py
+++
@@ -1,8 +0,0 @@
-import requests
-res = requests.options(
-    "http://127.0.0.1:8002/api/v1/favorites/menu-items/1",
-    headers={"Origin": "http://localhost:3000", "Access-Control-Request-Method": "POST"}
-)
-print(res.status_code)
-print(res.headers)
-print(res.text)
